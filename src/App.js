d96c1aca8fdbeeec1763722e10f39d25d0221e0a
Vishnu Murthy <vishnu.murthy@sincera.com>
2025-06-25 23:59:45 +0530

    Initial commit for Date Math Calculator

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import {
+  addDays,
+  addMonths,
+  addYears,
+  subDays,
+  subMonths,
+  subYears,
+  differenceInDays,
+  differenceInMonths,
+  differenceInYears,
+  intervalToDuration,
+  format,
+  isWeekend
+} from "date-fns";
+
+export default function DateMathCalculator() {
+...
+}
